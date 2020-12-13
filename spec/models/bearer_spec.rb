3cf3867e9f1b464b9d816afae1970f3930f53513
Yury Polyakov <x3qt.dev@gmail.com>
2020-12-13 21:28:28 +0300

    Add bearer name uniqueness spec

--- spec/models/bearer_spec.rb
+++ spec/models/bearer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Bearer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#name" do
+    context "uniqueness" do
+      let(:name) { "some cool name" }
+      let(:create_bearer) { create(:bearer, name: name) }
+
+      before do
+        create_bearer
+      end
+
+      it "preserved" do
+        expect { create_bearer }.not_to change(Bearer, :count)
+      end
+    end
+  end
 end
